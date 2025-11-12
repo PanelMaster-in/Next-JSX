e6721b1b0b7aa31cdf5e270b10cbdd1476784d38
Anuraag <panelmaster.in@gmail.com>
2025-11-12 13:48:24 +0530

    Initial

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,13 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  distDir: 'rendered',
+  devIndicators: false
 };
 
 export default nextConfig;
